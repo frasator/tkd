e75d1ebbf2b6946cbfd92a859d091d1aa0a67661
Paco <frasator@gmail.com>
2019-06-19 13:24:09 +0200

    Changes.

---
+++ tecnicas.js
@@ -0,0 +1,339 @@
+window.tecnicas = {
+    "blanco-amarillo": {
+        "sogui": [
+            "Orun Sogui",
+            "Uen Sogui"
+        ],
+        "maki": [
+            "Are Maki",
+            "Momtong Maki"
+        ],
+        "gongkiok": [
+            "Momtong Bande Jirugui"
+        ],
+        "pum": [
+            "Chariot"
+        ],
+        "chagui": [
+            "Ap Chagui",
+            "Bandal Chagui"
+        ]
+    },
+    "amarillo": {
+        "sogui": [
+            "Ap Sogui",
+            "Ap Kubi Sogui"
+        ],
+        "maki": [
+            "Momtong An Maki",
+            "Olgul Maki"
+        ],
+        "gongkiok": [
+            "Momtong Baro Jirugui",
+            "Olgul Bande Jirugui",
+        ],
+        "pum": [
+            "Kibon Chumbi",
+        ],
+        "chagui": [
+            "Yop Chagui",
+            "Dollyo Chagui",
+        ]
+    },
+    "amarillo-naranja": {
+        "sogui": [
+            "Chuchum Sogui"
+        ],
+        "maki": [
+            "Jansonnal Momtong Bakat Maki",
+        ],
+        "gongkiok": [
+            "Momtong Dubong Jirugui",
+        ],
+        "pum": [],
+        "chagui": [
+            "Tuit Chagui"
+        ]
+    },
+    "naranja": {
+        "sogui": [
+            "Tuit Kubi Sogui",
+        ],
+        "maki": [
+            "Sonnal Momtong Maki",
+        ],
+        "gongkiok": [
+            "Sonnal An Chigui",
+            "Pioson Kut Seuo Chirugui",
+        ],
+        "pum": [
+            "Chebipum Mok Chigui",
+        ],
+        "chagui": [
+            "Nacko Chagui",
+            "Neryo Chagui",
+        ]
+    },
+    "naranja-verde": {
+        "sogui": [
+            "Pionji Sogui"
+        ],
+        "maki": [
+            "Momtong Bakat Maki"
+        ],
+        "gongkiok": [
+            "Dung Chumok Ape Chigui"
+        ],
+        "pum": [],
+        "chagui": [
+            "Furio Chagui",
+            "Bakat Dari Furigui",
+            "An Dari Furigui"
+        ]
+    },
+    "verde": {
+        "sogui": [
+            "Tuit Koa Sogui"
+        ],
+        "maki": [],
+        "gongkiok": [
+            "Me chumok Neryo Yop Chigui",
+            "Palkup Dollyo Chigui"
+        ],
+        "pum": [
+            "Momtong Piochok Palkup Chigui"
+        ],
+        "chagui": [
+            "Mom Dollyo Yop Chagui",
+            "Mom Dollyo Tuit Chagui"
+        ]
+    },
+    "verde-azul": {
+        "sogui": [
+            "Naranji Sogui"
+        ],
+        "maki": [
+            "Olgul Bakat Maki",
+            "Gechio Are maki",
+            "Batagson Momtong Maki"
+        ],
+        "gongkiok": [],
+        "pum": [
+            "Bituro Jansonnal Olgul Bakat Maki"
+        ],
+        "chagui": [
+            "Mom Dollyo Nacko Chagui"
+        ]
+    },
+    "azul": {
+        "sogui": [
+            "Moa Sogui",
+            "Bom Sogui"
+        ],
+        "maki": [
+            "Batagson Momtong An Maki",
+            "Sonnal Are Maki",
+            "Okgoro Are Maki"
+        ],
+        "gongkiok": [
+            "Yop Jirugui",
+        ],
+        "pum": [
+            "Bo Chumok Chumbi"
+        ],
+        "chagui": [
+            "Mom Dollyo Furio Chagui",
+            "Tuio Ap Chagui"
+        ]
+    },
+    "azul-rojo": {
+        "sogui": [],
+        "maki": [
+            "Goduro Batagson Momtong An Maki",
+            "Gechio Momtong Maki"
+        ],
+        "gongkiok": [
+            "Dung Chumok Bakat Chigui",
+            "Du Checho Jirugui",
+        ],
+        "pum": [
+            "Gawi Maki",
+            "Dangkio Ollyo Murup Chigui",
+            "Piochok Chagui"
+        ],
+        "chagui": [
+            "Tuio Yop Chagui",
+            "Tuio Dollyo Chagui"
+        ]
+    },
+    "rojo": {
+        "sogui": [
+
+        ],
+        "maki": [
+            "Jansonnal Momtong Yop Maki",
+            "Goduro Momtong Maki",
+            "Goduro Are Maki",
+        ],
+        "gongkiok": [
+
+        ],
+        "pum": [
+            "Oe Santul Maki",
+            "Dangkio Tok Jirugui"
+        ],
+        "chagui": [
+            "Tuio Mom Dollyo Tuit Chagui",
+            "Tuio Mom Dollyo Yop Chagui"
+        ]
+    },
+    "segundo-dan": {
+        "sogui": [
+            "Jakdari Sogui"
+        ],
+        "maki": [
+            "Jansonnal Are Maki",
+            "Nulo Maki",
+            "An Palmok Gechio Momtong Maki",
+            "Jansonnal Momtong Maki"
+        ],
+        "gongkiok": [
+            "Sonnal Bakat Chigui",
+            "Palkup Yop Chigui",
+            "Sonnal Mok Chigui"
+        ],
+        "pum": [
+            "Tong Milgui Chumbi",
+            "Akumson Kalchebi",
+            "Murup Koki",
+            "Piochok Jirugui",
+            "Chagun Dol Chogui",
+            "Dangkio Are Pionson Kut Checho Chirugui",
+            "Me Chumok Are Piochok Chigui",
+            "Batagson Tok Chigui",
+            "Kumgang Maki",
+            "Kun Dol Chogui",
+            "Santul Maki"
+        ],
+        "chagui": [
+            "Tuio Mom Dollyo Nacko Chagui",
+            "Tuio Mom Dollyo Furio Chagui"
+        ]
+    },
+    "tercer-dan": {
+        "sogui": [
+            "Ap Koa Sogui"
+        ],
+        "maki": [
+            "Sonnal Gechio Are Maki",
+            "An Palmok Goduro Olgul Yop Maki"
+        ],
+        "gongkiok": [
+            "Palkup Ollyo Chigui"
+        ],
+        "pum": [
+            "Kolcho Maki",
+            "An Palmok Kumgang Momtong Maki",
+            "Kiopson Chumbi",
+            "Mohe Chigui",
+            "Gechio Santul Maki",
+            "Dangkio Dung Chumok Ape Chigui"
+        ],
+        "chagui": [
+            "Tuio Noppy Chagui",
+            "Tuio Gawi Chagui"
+        ]
+    },
+    "cuarto-dan": {
+        "sogui": [
+        ],
+        "maki": [
+            "Sonnal Dung Gechio Momtong Maki",
+            "Goduro Sonnal Dung Momtong Bakat Maki",
+            "Sonnal Okgoro Are Maki",
+            "An Palmok Momtong Bakat Maki",
+            "Jansonnal Olgul Maki",
+            "Yop Are Maki",
+            "An Palmok Goduro Momtong Maki"
+        ],
+        "gongkiok": [
+            "Pionson Kut Opo Chirugui",
+            "Bam Chumok Sosum Chigui"
+        ],
+        "pum": [
+            "Hwangso Maki",
+            "Sonbadak Goduro An Palmok Momtong Bakat Maki",
+            "Bawi Milgui",
+            "Gorro Olligui",
+            "Chetdari Jirugui",
+            "Kumgang Ap Jirugui",
+            "Me Chumok Yop Momtong Piochok Chigui",
+            "Nalgue Piogui",
+            "Bituro Jansonnal Momtong Bakat Maki",
+            "An Palmok Bituro Momtong Bakat Maki",
+            "Kumgang Yop Jirugui",
+            "Sonnal Oe Santul Maki",
+            "Taesan Milgui"
+        ],
+        "chagui": [
+            "Tuio Ilya Chagui",
+            "Modumbal Ap Chagui",
+            "Modumbal Yop Chagui"
+        ]
+    },
+    "quinto-dan": {
+        "sogui": [
+            "Kiotari Sogui",
+            "Ogum Sogui"
+
+        ],
+        "maki": [
+            "Okgoro Olgul Maki",
+            "Sonnal Oe Santul Maki"
+        ],
+        "gongkiok": [
+            "Du Me Chumok Yang Yopguri Chigui"
+        ],
+        "pum": [
+            "An Palmok Are Piochok Maki",
+            "Sonnal Kumgang Are Maki",
+            "Oe Santul Yop Chagui",
+            "Du Chumok Jori Chumbi"
+        ],
+        "chagui": [
+        ]
+    }
+}
+
+window.NUMERO_PREGUNTAS = {
+    'sogui': {
+        'segundo-dan': 4,
+        'tercer-dan': 4,
+        'cuarto-dan': 5,
+        'quinto-dan': 5
+    },
+    'maki': {
+        'segundo-dan': 4,
+        'tercer-dan': 4,
+        'cuarto-dan': 5,
+        'quinto-dan': 5
+    },
+    'gongkiok': {
+        'segundo-dan': 4,
+        'tercer-dan': 4,
+        'cuarto-dan': 5,
+        'quinto-dan': 5
+    },
+    'pum': {
+        'segundo-dan': 3,
+        'tercer-dan': 4,
+        'cuarto-dan': 4,
+        'quinto-dan': 4
+    },
+    'chagui': {
+        'segundo-dan': 4,
+        'tercer-dan': 4,
+        'cuarto-dan': 5,
+        'quinto-dan': 5
+    }
+}
